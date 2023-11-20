b912279b637767e4fc8ae71912f084bfbe763e13
Dave Augustus <davetweetlive@gmail.com>
2023-11-20 23:21:29 +0530

    Forloop
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

---
+++ lesson015/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Infinite loop
+func main() {
+	for {
+		fmt.Println("Hello World!")
+	}
+}
---
+++ lesson016/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Infinite loop
+func main() {
+	var i int
+
+	for i = 0; i < 10; i++ {
+		fmt.Println("Hello World!")
+	}
+}
