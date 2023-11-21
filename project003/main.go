6dc884df75207fda66d7bb815f6b2730c21362ba
Dave Augustus <davetweetlive@gmail.com>
2023-11-22 00:07:07 +0530

    Factorial
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- project003/main.go
+++ project003/main.go
@@ -2,18 +2,34 @@ package main
 
 import "fmt"
 
-// Swapped numbers
+// Factorial of a numbers
 func main() {
-	a := 5
+	var num int
 
-	b := 9
+	fmt.Printf("Enter a number: ")
+	fmt.Scanln(&num)
 
-	var temp int
+	if num < 0 {
+		fmt.Println("Invalid input! Please enter a positive integer")
+		return
+	}
 
-	temp = a
-	a = b
-	b = temp
+	result := Factorial(num)
 
-	fmt.Printf("a: %v\n", a)
-	fmt.Printf("b: %v\n", b)
+	fmt.Printf("Factorial of %v is %v!\n", num, result)
+
+}
+
+func Factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	fact := 1
+
+	for i := 1; i <= n; i++ {
+		fact = fact * i
+	}
+
+	return fact
 }
