5ad3bafadafabd65d2855e67c8de059d5a7792e6
Dave Augustus <davetweetlive@gmail.com>
2023-11-21 23:16:51 +0530

    Reverse a number
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- project002/main.go
+++ project002/main.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
 // TODO: Reverse a number
 func main() {
-	// number := 123
+	number := -123
+
+	reverse := 0
+	for number != 0 {
+		rem := number % 10
+		reverse = reverse*10 + rem
+		number = number / 10
+	}
+
+	fmt.Println(reverse)
 }
